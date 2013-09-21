e1cf88387f81fda35e791adc8c877397fe51866c
Ptr13 <shadowfax10000@gmail.com>
2013-09-20 20:46:20 -0500

    Update stuff

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+    
     $("#rotate").click(function() {
-        $("body").css("-webkit-transform","rotate(360deg)");
+        $("body").css({"-webkit-transform":"rotate(360deg)","transition":"3s","transform":"rotate(360deg)","-ms-transform":"rotate(360deg)","-webkit-transition":"");
             setTimeout(function() {
                 location.reload()
             }, 3000);
     });
+    
 });
