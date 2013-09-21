57e281fb66155342096c745682d42a434aa07a56
Ptr13 <shadowfax10000@gmail.com>
2013-09-21 18:11:27 -0500

    Update a lot of stuff
    Give website new look

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
-    
     $("#rotate").click(function() {
-        $("body").css({"-webkit-transform":"rotate(360deg)","transition":"3s","transform":"rotate(360deg)","-ms-transform":"rotate(360deg)","-webkit-transition":"");
-            setTimeout(function() {
-                location.reload()
-            }, 3000);
+        $("body").css({"-webkit-transform":"rotate(360deg)","transition":"2s"});
+        setTimeout(function() {
+            location.reload();
+        },2000);
     });
     
+    $("#about").hover(function() {
+        $(this).css("color","red"); 
+    },function() {
+        $(this).css("color","white");
+    });
+    
+    $("#demos").hover(function() {
+        $(this).css("color","green");
+    },function() {
+        $(this).css("color","white");
+    });
+    
+    $("#css").hover(function() {
+        $(this).css("color","blue");
+    },function() {
+        $(this).css("color","white");
+    });
+    
+    $("#rotate").hover(function() {
+        $(this).css("color","purple");
+    },function() {
+        $(this).css("color","white");
+    });
 });
+
+
