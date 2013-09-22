ecbfaaa92533cc3c9f540cd6e7fda7a5ec9ce57e
Peter Davis <shadowfax10000@gmail.com>
2013-09-21 21:44:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".hidden").show("bounce",{times: 2},1000);
+    $(".hidden").show("bounce",{times: 2},2000);
     
     $("#rotate").click(function() {
         $("body").css({"-webkit-transform":"rotate(360deg)","transition":"2s"});
