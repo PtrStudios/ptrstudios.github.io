8fc2b681595a85c7779f1569bfcc7fe900e298ff
PtrStudios <gandalfthewhite@outlook.com>
2013-10-05 16:38:38 -0500

    Update style

--- script.js
+++ script.js
@@ -34,11 +34,26 @@ $(document).ready(function() {
 
     var hcanvas = $("#hcanvas")[0];
     var hctx = hcanvas.getContext("2d");
+    function random() {
+        c1 = Math.floor(Math.random() * 255);
+        c2 = Math.floor(Math.random() * 255);
+        c3 = Math.floor(Math.random() * 255);
+        c4 = Math.floor(Math.random() * 255);
+        c5 = Math.floor(Math.random() * 255);
+        c6 = Math.floor(Math.random() * 255);
+    }
+    random();
     var grd = hctx.createLinearGradient(0, 0, hcanvas.width, 0);
-    grd.addColorStop(0, "red");
-    grd.addColorStop(1, "blue");
+    grd.addColorStop(0, "rgb(" + c1 + "," + c2 + "," + c3 + ")");
+    grd.addColorStop(1, "rgb(" + c4 + "," + c5 + "," + c6 + ")");
     hctx.fillStyle = grd;
     hctx.fillRect(0, 0, hcanvas.width, hcanvas.height);
-    
+    setInterval(function () {
+        random();
+        grd.addColorStop(0, "rgb(" + c1 + "," + c2 + "," + c3 + ")");
+        grd.addColorStop(1, "rgb(" + c4 + "," + c5 + "," + c6 + ")");
+        hctx.fillStyle = grd;
+        hctx.fillRect(0, 0, hcanvas.width, hcanvas.height);
+    },1000);
 });
 
